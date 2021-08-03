37b8e0748a26d2650b5acf4d6546b8632d1e0cd2
Valerii Radchenko <valeravivaz@gmail.com>
2021-08-03 14:36:58 +0300

    format code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,46 @@
 const COFF_PREM = 1.25;
 
-class Worker{
-  constructor(name, lastname, days, salaryPerDey, getPremium){
-    
-    if(typeof (name && lastname) === 'string'){
+
+class Worker {
+  constructor(name, lastname, days, salaryPerDey, getPremium) {
+    if (typeof (name && lastname) === "string") {
       this.name = name;
       this.lastname = lastname;
     } else {
-      throw new TypeError('Name and lastname data must be "string" type')
+      throw new TypeError('Name and lastname data must be "string" type');
     }
 
-    if (typeof (days && salaryPerDey)==='number'){
-
-      if(days >=0 && salaryPerDey >=0){ 
+    if (typeof (days && salaryPerDey) === "number") {
+      if (days >= 0 && salaryPerDey >= 0) {
         this.days = days;
         this.salaryPerDey = salaryPerDey;
       } else {
-        throw new RangeError ('Days and salaryPerDay data must be positive value')
+        throw new RangeError(
+          "Days and salaryPerDay data must be positive value"
+        );
       }
-
     } else {
-      throw new TypeError('Bays and salaryPerDay data type must be "NUMBER"')}
-      
-    if(typeof getPremium === 'boolean'){
+      throw new TypeError('Bays and salaryPerDay data type must be "NUMBER"');
+    }
+
+    if (typeof getPremium === "boolean") {
       this.getPremium = getPremium;
     } else {
-      throw new TypeError('getPremium data must be boolean')
+      throw new TypeError("getPremium data must be boolean");
     }
   }
 
-  getFullName(){
-    return `${this.name} ${this.lastname}`
+  getFullName() {
+    return `${this.name} ${this.lastname}`;
   }
 
-  getSalary(){
-      if(this.getPremium){
-      return this.salaryPerDey*this.days*COFF_PREM;
+  getSalary() {
+    if (this.getPremium) {
+      return this.salaryPerDey * this.days * COFF_PREM;
     }
-    return this.days*this.salaryPerDey;
+    return this.days * this.salaryPerDey;
   }
 }
 
-const Oleg = new Worker('Oleg', 'NeOleg', 12, 2000, true);
-const Jack = new Worker('Jack', 'BadWorker', 6, 600, false); 
-
+const Oleg = new Worker("Oleg", "NeOleg", 12, 2000, true);
+const Jack = new Worker("Jack", "BadWorker", 6, 600, false);
